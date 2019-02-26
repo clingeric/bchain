882380abc12285f567cb14f0c4b7f65376ad063b
Eric Clinger <cling.eric@gmail.com>
2019-02-25 17:27:45 -0700

    added login

--- homepage/views/login.py
+++ homepage/views/login.py
@@ -1,41 +1,46 @@
 from django.contrib.auth import authenticate,login
-# from account.models import User
 from django_mako_plus import view_function
 from django.http import HttpResponseRedirect
 from django import forms
+from formlib import Formless
 import re
-
 
 @view_function
 def process_request(request):
+    
+    form = SignupForm(request)
 
-#    form = SignupForm(request)
+    if form.is_valid():
+        # form.commit()
+        if(form.username == 'producer'):
+            return HttpResponseRedirect('/producer')
+        elif(form.username == 'manufacturer'):
+            return HttpResponseRedirect('/manufacturer')
+        elif(form.username == 'distributor'):
+            return HttpResponseRedirect('/distributor')
+        elif(form.username == 'shipper'):
+            return HttpResponseRedirect('/shipper')
+        elif(form.username == 'retailer'):
+            return HttpResponseRedirect('/retailer')
 
-#    if form.is_valid():
-#        form.commit()
-#        return HttpResponseRedirect('/')
+    context = {
+            'form': form,
+        }  
 
-   context = {
-    #    'form': form,
-   }
+    return request.dmp.render('login.html', context)
 
-   return request.dmp.render('login.html', context)
+class SignupForm(Formless):
 
-# class SignupForm(forms):
+    def init(self):
+       '''Adds the fields for the form'''
+       self.fields['Username'] = forms.CharField(label='Username')
+       self.fields['Password'] = forms.CharField(label='Password', widget=forms.PasswordInput)
 
-#    def init(self):
-#        '''Adds the fields for the form'''
-#        self.fields['email'] = forms.CharField(label='Email :')
-#        self.fields['password'] = forms.CharField(label='Password :', widget=forms.PasswordInput)
-#        self.user = None
+    def clean(self):
+        self.username = self.cleaned_data.get('Username')
+        self.password = self.cleaned_data.get('Password')
+        # return self.cleaned_data
 
-#    def clean(self):
-#        '''Authenticates the user to check for valid credentials'''
-#        self.user = authenticate(email=self.cleaned_data.get('email'), password=self.cleaned_data.get('password'))
-#        if self.user is None:
-#            raise forms.ValidationError('Invalid email or password.')
-#        return self.cleaned_data
-
-
-#    def commit(self):
-#        login(self.request, self.user)
+    def commit(self):
+        return HttpResponseRedirect('google.com')
+        
