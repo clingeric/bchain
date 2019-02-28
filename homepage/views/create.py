fa3fdba30af70be9b8e2b0317237a5af4fd453cd
Nicole Tucker <nicoletucker@Nicoles-MacBook-Pro-2.local>
2019-02-28 09:04:01 -0700

    finished styling for login, and all portal pages

--- homepage/views/create.py
+++ homepage/views/create.py
@@ -1,19 +1,22 @@
 from django.conf import settings
 from django_mako_plus import view_function, jscontext
-from datetime import datetime, timezone
 
 @view_function
 def process_request(request):
-    utc_time = datetime.utcnow()
     context = {
-        # sent to index.html:
-        'utc_time': utc_time,
-        # sent to index.html and index.js:
-        jscontext('utc_epoch'): utc_time.timestamp(),
+        
     }
     return request.dmp.render('create.html', context)
 
+# def create_asset():
 '''
-notes:
-how do we assure that quality isn't made up? do we have multiple parties examine grade?
+the initial entry into the blockchain of the asset
+adds all the necessary info about the asset
+'''
+
+# def send_asset():
+'''
+sends the asset off to the manufacturer, not using any shipper
+removes responsibility of the asset from the producer
+gives responsibility to the manufacturer
 '''
